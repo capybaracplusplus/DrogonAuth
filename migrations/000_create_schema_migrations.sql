e38d926f92bdb61a91b78c4e9788db3acc92e89c
Dima <dimak12k@yandex.ru>
2025-01-15 05:35:46 +0300

    fix create schema file

--- migrations/000_create_schema_migrations.sql
+++ migrations/000_create_schema_migrations.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS schema_migrations (
-                                                 id SERIAL PRIMARY KEY,
-                                                 version VARCHAR(255) NOT NULL UNIQUE,
+    id SERIAL PRIMARY KEY,
+    version VARCHAR(255) NOT NULL UNIQUE,
     applied_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     );
